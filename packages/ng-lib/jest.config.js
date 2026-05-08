677b05e1ab3a19025be8bf87e03325e8eb92f1e9
Michael Masterson <mgmastersonjr@gmail.com>
2026-05-07 23:06:08 -0300

    chore: testing fix

---
+++ packages/ng-lib/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  passWithNoTests: true,
+  modulePathIgnorePatterns: ['<rootDir>/../../dist/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@angular|@smithy|rxjs)/)',
+  ],
+};
