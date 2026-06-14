7090ce4917b704ee0617a26494abd726ee8baf5e
Michael Masterson <mgmastersonjr@gmail.com>
2026-06-14 09:49:41 -0300

    chore: storybook theme toggle

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,19 @@
 import type { Preview } from '@storybook/html';
+import { applyTheme, applyColorMode, sharedGlobalTypes } from '../packages/storybook-shared/src';
 
 const preview: Preview = {
+  globalTypes: sharedGlobalTypes,
+
+  decorators: [
+    (_story, context) => {
+      applyTheme(context.globals['brandTheme'] ?? 'm2s2');
+      applyColorMode(context.globals['colorMode'] ?? 'dark');
+      return _story();
+    },
+  ],
+
   parameters: {
+    backgrounds: { disable: true },
     layout: 'padded',
   },
 };
