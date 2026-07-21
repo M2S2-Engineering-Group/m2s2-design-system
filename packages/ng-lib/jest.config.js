8e439b8a9742379fa8801677ca6c19effc7447b2
Michael Masterson <mgmastersonjr@gmail.com>
2026-07-21 13:31:30 -0400

    fix: storybook angular deploy failure

--- packages/ng-lib/jest.config.js
+++ packages/ng-lib/jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(@angular|@smithy|rxjs|@testing-library|marked)/)',
   ],
-  setupFilesAfterEnv: ['<rootDir>/packages/ng-lib/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@m2s2/models$': '<rootDir>/packages/models/src/index.ts',
-    '^@m2s2/utils$': '<rootDir>/packages/utils/src/index.ts',
-    '^@m2s2/utils/testing$': '<rootDir>/packages/utils/src/testing.ts',
+    '^@m2s2/models$': '<rootDir>/../models/src/index.ts',
+    '^@m2s2/utils$': '<rootDir>/../utils/src/index.ts',
+    '^@m2s2/utils/testing$': '<rootDir>/../utils/src/testing.ts',
   },
 };
